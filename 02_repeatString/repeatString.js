063b7a7ae87a220172ab9b7016adb58f00a07521
Bill Grenard <williamgrenard@gmail.com>
2024-02-04 20:41:31 -0800

    Complete 02_repeatString assignment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let initialString = str;
 
+    if (num > 0) {
+        for (let i = 0; i < num - 1; i++) {
+            str += initialString;
+        }
+    }
+    else if (num == 0) {
+        return "";
+    }
+    else {
+        return "ERROR"
+    }
+    return str;
 };
 
 // Do not edit below this line
