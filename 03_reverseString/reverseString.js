383082e9435e2cee37c3a19fc40ae48c919d342d
Bill Grenard <williamgrenard@gmail.com>
2024-02-06 19:28:41 -0800

    Complete 03_reverseString assignment

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    let inputReversed = "";
 
+    for (let i = inputString.length - 1; i >= 0; i--) {
+        inputReversed += inputString[i];
+    }
+
+    return inputReversed
 };
 
 // Do not edit below this line
