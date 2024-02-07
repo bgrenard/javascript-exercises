3534c7c0ce964f0f8181d21897d1f17673ed779d
Bill Grenard <williamgrenard@gmail.com>
2024-02-06 20:19:05 -0800

    Complete 04_removeFromArray assignment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    let newArray = [];
+    
+    for (const x of inputArray) {
+        if (!elementsToRemove.includes(x)) {
+            newArray.push(x);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
