ec445b6e264562e456c2b3b63a887a9c763e532d
Bill Grenard <williamgrenard@gmail.com>
2024-02-07 21:37:48 -0800

    Complete 05_sumAll assignment

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    else if (!(typeof a === 'number' && typeof b === 'number')) {
+        return "ERROR"
+    }
 
+    else {
+        let first = Math.min(a, b);
+        let last = Math.max(a,b)
+        return 0.5*(last - first + 1)*(last + first);
+    }
 };
 
 // Do not edit below this line
