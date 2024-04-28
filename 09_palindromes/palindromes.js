109b0146637448cd6901153e1126b95639c304db
Bill Grenard <williamgrenard@gmail.com>
2024-04-27 19:29:57 -0700

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    string = string.toLowerCase();
+
+    let noPunct = "";
+
+    for (let i = 0; i < string.length; i++) {
+        //We need to remove spaces and punctuation from the string before checking for palindrome.
+        if (string.charAt(i) !== ' ' &&
+            string.charAt(i) !== '.' &&
+            string.charAt(i) !== '!' &&
+            string.charAt(i) !== '?' &&
+            string.charAt(i) !== ','
+        ) {
+            noPunct += string.charAt(i);
+            }
+    }
+
+    const firstHalf = noPunct.substring(0, Math.floor(noPunct.length/2));
+    let secondHalf = "";
+
+    if (noPunct.length % 2 === 0) {
+        secondHalf = noPunct.substring(Math.floor(noPunct.length/2));
+    }
+    else {
+        secondHalf = noPunct.substring(Math.floor(noPunct.length/2) + 1);
+    }
+
+    const secondHalfReversed = secondHalf.split("").reduce((total, current) => current + total, "");
+
+    return firstHalf === secondHalfReversed;
 };
 
 // Do not edit below this line
