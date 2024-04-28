caa63d6c7f90042b8f2ec193966073d0846aac2c
Bill Grenard <williamgrenard@gmail.com>
2024-04-27 19:57:11 -0700

    Improve palindromes by using regexp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,6 @@
 const palindromes = function (string) {
-
-    string = string.toLowerCase();
-
-    let noPunct = "";
-
-    for (let i = 0; i < string.length; i++) {
-        //We need to remove spaces and punctuation from the string before checking for palindrome.
-        if (string.charAt(i) !== ' ' &&
-            string.charAt(i) !== '.' &&
-            string.charAt(i) !== '!' &&
-            string.charAt(i) !== '?' &&
-            string.charAt(i) !== ','
-        ) {
-            noPunct += string.charAt(i);
-            }
-    }
-
-    const reversed = noPunct.split("").reduce((total, current) => current + total, "");
-
-    return noPunct === reversed;
+    const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return processedString.split("").reverse().join("") === processedString;
 };
 
 // Do not edit below this line
