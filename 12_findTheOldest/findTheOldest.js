7e5ff060a708857752933709b761a20e84f1a5c9
Bill Grenard <williamgrenard@gmail.com>
2024-04-27 21:38:11 -0700

    Add solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sorted = people.sort((a,b) => {
+        let ageA;
+        let ageB;
+        
+        if (typeof a.yearOfDeath !== 'undefined') {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        else {
+            const currentYear = new Date().getFullYear();
+            ageA = currentYear - a.yearOfBirth;
+        }
 
+        if (typeof b.yearOfDeath !== 'undefined') {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+        else {
+            const currentYear = new Date().getFullYear();
+            ageB = currentYear - b.yearOfBirth;
+        }
+
+        return ageB - ageA;
+    });
+    return sorted[0];
 };
 
 // Do not edit below this line
